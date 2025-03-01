6871bdf1d8554fa9e585bb53562cb20cf2a649fe
Joao Vieira <joao.dev.599@gmail.com>
2025-03-01 18:42:54 -0300

    Actions: Sobrescrevendo metodos

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,3 +1,4 @@
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from  .serializers import PontoTuristicoSerializer
@@ -10,3 +11,33 @@ class PontoTuristicoViewSet(ModelViewSet):
     def get_queryset(self):
         return PontoTuristico.objects.filter(aprovado=True) 
     
+    # GET
+    # def list(self, request, *args, **kwargs):
+    #     return Response(
+    #         {
+    #             'teste': 'Sobrescrevendo GET'
+    #         }
+    #     )
+
+    # POST
+    # def create(self, request, *args, **kwargs):
+    #     return Response(
+    #         {
+    #             'Hello': request.data['nome']
+    #         }
+    #     )
+
+    # DELETE
+    # def destroy(self, request, *args, **kwargs):
+    # pass
+    
+    # def retrieve(self, request, *args, **kwargs):
+    #     pass
+
+    # PUT
+    # def update(self, request, *args, **kwargs):
+    #     pass
+
+    # PATH
+    # def partial_update(self, request, *args, **kwargs):
+    #     pass
