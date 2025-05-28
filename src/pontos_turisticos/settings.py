51ed05ca31633de2323f16a0bbaa34ea79ab36f8
Joao Vieira <joao.dev.599@gmail.com>
2025-05-27 21:18:43 -0300

    Docs: Swagger integration

--- src/pontos_turisticos/settings.py
+++ src/pontos_turisticos/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'django_filters',
+    'drf_spectacular',
     'core',
     'atracoes',
     'comentarios',
@@ -147,6 +148,7 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10, 
 
--- src/pontos_turisticos/urls.py
+++ src/pontos_turisticos/urls.py
@@ -9,17 +9,27 @@ from atracoes.api.viewsets import AtracaoViewSet
 from enderecos.api.viewsets import EnderecoViewSet
 from comentarios.api.viewsets import ComentarioViewSet
 from avaliacoes.api.viewsets import AvaliacaoViewSet
-
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 router = routers.DefaultRouter()
 router.register(r'atracoes', AtracaoViewSet)
 router.register(r'enderecos', EnderecoViewSet)
 router.register(r'avaliacoes', AvaliacaoViewSet)
 router.register(r'comentarios', ComentarioViewSet)
-router.register(r'pontoturistico', PontoTuristicoViewSet, basename='PontoTurustico')
+router.register(r'pontoturistico', PontoTuristicoViewSet, basename='PontoTuristico')
 
 
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+
+     # Schema OpenAPI
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    
+    # Documentação Swagger
+    path('api/schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    
+    # Documentação Redoc
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
